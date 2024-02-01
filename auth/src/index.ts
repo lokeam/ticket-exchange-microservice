f7f534ff16e344d50ba6f231808c6dc3b0f046ca
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-31 21:56:08 -0500

    build: ingress-nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (request, response) => {
+  response.send('Testing');
+});
+
 app.listen(3000, () => {
   console.log('Huzzah! Listening on port 3000!');
 });
