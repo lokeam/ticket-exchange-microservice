6cef7cfcf00aca2753ba4903b01da7cc2ad51eb0
Ahn Ming Loke <lokeahnming@gmail.com>
2024-02-01 11:07:35 -0500

    feat: add express-validator for post validation within signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (request, response) => {
-  response.send('testing from signup');
+router.post('/api/users/signup',
+[
+  body('email')
+    .isEmail()
+    .withMessage('You must enter a valid Email'),
+  body('password')
+    .trim()
+    .withMessage('Your Password must be between 10 and 30 chars')
+],
+(request: Request, response: Response) => {
+  const { email, password } = request.body;
+
+
 });
 
 export { router as signupRouter };
