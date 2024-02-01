50880e079fff01ea361e638244c8e922ce481ed5
Ahn Ming Loke <lokeahnming@gmail.com>
2024-02-01 12:18:00 -0500

    feat: add minLength handling to password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,20 +1,24 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
 router.post('/api/users/signup',
 [
-  body('email')
-    .isEmail()
-    .withMessage('You must enter a valid Email'),
+  body('email').isEmail().withMessage('You must enter a valid Email'),
   body('password')
     .trim()
+    .isLength({ min: 10, max: 30 })
     .withMessage('Your Password must be between 10 and 30 chars')
 ],
 (request: Request, response: Response) => {
-  const { email, password } = request.body;
+  const errors = validationResult(request);
 
+  if (!errors.isEmpty()) {
+    return response.status(400).send(errors.array());
+  }
+  console.log('Creating a user...');
+  response.send({});
 
 });
 
